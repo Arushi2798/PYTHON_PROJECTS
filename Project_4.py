e8b71e5e2104cbc6511ee475beb8af2e09aee495
Arushi2798 <arushi1301@gmail.com>
2025-01-31 20:26:03 +0530

    new lines

--- Project_4.py
+++ Project_4.py
@@ -1,2 +1,4 @@
 #Question Statement
-"""Creating a Caesar Cipher: an encryption technique."""
+"""Creating a Caesar Cipher: an encryption technique.
+a key is set which helps shift the alphabets of the text for encryption , which is turn will be used to decrypt the text message."""
+
