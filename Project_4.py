ed669056dd7ffbfff07d10736d60cac1b04d91a5
Arushi2798 <arushi1301@gmail.com>
2025-02-01 22:15:31 +0530

    new lines

--- Project_4.py
+++ Project_4.py
@@ -1,4 +1,39 @@
 #Question Statement
 """Creating a Caesar Cipher: an encryption technique.
-a key is set which helps shift the alphabets of the text for encryption , which is turn will be used to decrypt the text message."""
+a key is set which helps shift the alphabets of the text for encryption,
+which is turn will be used to decrypt the text message."""
 
+d=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+final=""
+
+def encrypt(sentence, key):
+    # print("en ans")
+    for each in sentence:
+        if each in d:
+            pos=d.index(each)
+            res=d[pos+key]
+    print(final+res)
+
+
+def decrypt(sentence,key):
+    # print('dc ans')
+    for each in sentence:
+        if each in d:
+            pos=d.index(each)
+            res=d[pos-key]
+    print(final+res)
+
+
+repeat="y"
+while repeat=="y":
+    ans=input("Type 'encrypt' for performing encryption and 'decrypt' for performing decryption: \n")
+    sen=input("Type your message: \n")
+    k=int(input("Type the shift number"))
+    
+    if ans== "encrypt":
+        encrypt(sen,k)
+                
+    elif ans =="decrypt":
+        decrypt(sen,k)
+    
+    repeat=input("Type 'y' if you wanna go again, otherwise type 'n'. ")
