9ba3f771f9a2e0b32a7ee6626e76509cf003324d
Arushi2798 <arushi1301@gmail.com>
2025-02-02 19:46:26 +0530

    new Project

--- Project_5.py
+++ Project_5.py
@@ -1,5 +1,5 @@
 #Question Statement
-"""Creating a A silent Auction Program: bidding is performed silently, where each other's bid is not known.
+"""Creating A silent Auction Program: bidding is performed silently, where each other's bid is not known.
 the highest bid wins."""
 
 
