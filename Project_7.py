5fa01b21059b813c68def8d84964569308489511
Arushi2798 <arushi1301@gmail.com>
2025-02-17 20:12:41 +0530

    code update

--- Project_7.py
+++ Project_7.py
@@ -14,20 +14,21 @@ elif level =='hard':
     print("you have 4 attempts to guess the number!!")
     chance=4
 
-while no_turn < chance:
-    c=chance
+while no_turn <= chance:
     no=int(input('make a guess: '))
     if no >guess:
         print("you guessed high")
-        c-=1
+        chance-=1
+        
     elif no< guess:
         print('you guessed low')
-        c-=1
+        chance-=1
+        
     elif no==guess:
         print("you are correct!!!")
         break     
 
-    print(f"you have {c} chances left")
+    print(f"you have {chance} chances left")
     print("guess again")
     no_turn+=1
 
