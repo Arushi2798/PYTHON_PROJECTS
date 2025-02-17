5666a5440d94151e2aca1f103d0ff4ff11861528
Arushi2798 <arushi1301@gmail.com>
2025-02-17 20:05:37 +0530

    code update

--- Project_7.py
+++ Project_7.py
@@ -2,25 +2,25 @@ import random
 print("Welcome to the GUESSING A NUMBER Game Folks!!")
 
 guess=random.randint(0,50)
-print("let me think of a number between 0 to 25.")
+print("let me think of a number between 0 to 50.")
 level=input("choose level of difficulty.... type 'easy' or 'hard'  ")
 print(guess)
 no_turn=0
 
 if level=='easy':
     print("you have 10 attempts to guess the number!!")
-    chance=5
+    chance=10
 elif level =='hard':
     print("you have 4 attempts to guess the number!!")
-    chance=2
+    chance=4
 
 while no_turn <= chance:
     no=int(input('make a guess: '))
     if no >guess:
-        print("you guessed too high")
+        print("you guessed high")
         chance-=1
     elif no< guess:
-        print('you guessed too low')
+        print('you guessed low')
         chance-=1
     elif no==guess:
         print("you are correct!!!")
@@ -28,3 +28,5 @@ while no_turn <= chance:
 
     print(f"you have {chance} chances left")
     no_turn+=1
+
+print("GAME OVER!!!")
