68e543e9319ff587654edd592c1336d6a666da81
Arushi2798 <arushi1301@gmail.com>
2025-02-17 20:34:32 +0530

    code update

---
+++ Project_8.py
@@ -0,0 +1,9 @@
+print("Welcome to the HIGHER LOWER GAME Folks!!")
+print('guess the no of online followers of the topic')
+
+d={"NASA,a space agency, from USA":10000,
+   "t-series, a music label, from india":20000,
+   "BTS, a band from soth korea":7000000000
+     }
+
+print(f"COMPARE {compare}")
