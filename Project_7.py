c11d3fe98116ea6dca61859f3e6f242a6515f615
Arushi2798 <arushi1301@gmail.com>
2025-02-17 20:27:14 +0530

    code update

--- Project_7.py
+++ Project_7.py
@@ -4,17 +4,17 @@ print("Welcome to the GUESSING A NUMBER Game Folks!!")
 guess=random.randint(0,50)
 print("let me think of a number between 0 to 50.")
 level=input("choose level of difficulty.... type 'easy' or 'hard'  ")
-print(guess)
-no_turn=0
 
 if level=='easy':
     print("you have 10 attempts to guess the number!!")
     chance=10
+
 elif level =='hard':
     print("you have 4 attempts to guess the number!!")
     chance=4
 
-while no_turn <= chance:
+
+while chance!=0:
     no=int(input('make a guess: '))
     if no >guess:
         print("you guessed high")
@@ -29,7 +29,5 @@ while no_turn <= chance:
         break     
 
     print(f"you have {chance} chances left")
-    print("guess again")
-    no_turn+=1
-
+    
 print("GAME OVER!!!")
