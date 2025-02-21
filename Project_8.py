2ab4c88a99bf5cbed5234bc3ef6faff60abf50a9
Arushi2798 <arushi1301@gmail.com>
2025-02-21 20:41:54 +0530

    code update

--- Project_8.py
+++ Project_8.py
@@ -3,7 +3,10 @@ print('guess the no of online followers of the topic')
 
 d={"NASA,a space agency, from USA":10000,
    "t-series, a music label, from india":20000,
-   "BTS, a band from soth korea":7000000000
+   "BTS, a band from soth korea":7000000000,
+   "Narendra Modi, a Prime Minister, from India":80000,
+   "Neeraj Chopra, an athlete from India":9000000,
+   "Jennifer Lopez, a Musician and actress, from USA":895647,
+   "Shakira, a Musician, from Colombia":795664,
+   'Justin Bieber, a Musician, from Canada ':46625,
      }
-compare=0
-print(f"COMPARE {compare}")
