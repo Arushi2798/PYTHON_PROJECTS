5b9a5653d99003cc904564c8110efc78fabb8aa8
Arushi2798 <arushi1301@gmail.com>
2025-02-22 22:58:27 +0530

    code update

--- Project_9.py
+++ Project_9.py
@@ -2,27 +2,39 @@ print("Welcome to the STARBUCKS COFFEE!!")
 choice=input("What would you like to have? (latte/espresso/cappuccino): ")
 print("Please insert coins now")
 
-coin=[5,10,20]
-money=0
-for each in coin:
-    coin=int(input(f"How many {each}Rs. coins? : "))
-    money+=coin*each
+def orderplaced():
+    coin=[5,10,20]
+    m=0
+    for each in coin:
+        coin=int(input(f"How many {each}Rs. coins? : "))
+        m+=coin*each
+    return m
 
-price={"latte":150,"espresso":200,"cappuccino":175}
-sp=-1
+money=orderplaced()
 
-match(choice):
-    case "latte":
-        sp=money-price.get("latte")
-    case "espresso":
-        sp=money-price.get("espresso")
-    case "cappuccino":
-        sp=money-price.get("cappuccino")
+def pricecal(money):
+    price={"latte":150,"espresso":200,"cappuccino":175}
 
-if sp<0:
-    print(f"please give Rs{-sp} more")
-elif sp>0 :
-    print(f"Here's your change of {sp} rupees")
-    print("Enjoy your drink")
-else:
-    print("Enjoy your drink")
+    match(choice):
+        case "latte":
+            sp=money-price.get("latte")
+        case "espresso":
+            sp=money-price.get("espresso")
+        case "cappuccino":
+            sp=money-price.get("cappuccino")
+
+    return sp
+
+sp=pricecal()
+
+def changecal(sp):
+    if sp<0:
+        print(f"please give Rs{-sp} more")
+        extra=orderplaced()
+        total=sp + pricecal(extra)
+        changecal(total)
+    elif sp>0 :
+        print(f"Here's your change of {sp} rupees")
+        print("Enjoy your drink")
+    else:
+        print("Enjoy your drink")
