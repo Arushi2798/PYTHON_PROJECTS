cd8b80ee4af3a19c4656102f94004c54e7e4967d
Arushi2798 <arushi1301@gmail.com>
2025-02-22 22:26:29 +0530

    code update

--- Project_9.py
+++ Project_9.py
@@ -1,6 +1,6 @@
 print("Welcome to the STARBUCKS COFFEE!!")
 choice=input("What would you like to have? (latte/espresso/cappuccino): ")
-print("Pleas einsert coins now")
+print("Please insert coins now")
 coin_5=int(input("How many 5Rs. coins? : "))
 coin_10=int(input("How many 10Rs. coins? : "))
 coin_20=int(input("How many 20Rs. coins? : "))
@@ -8,14 +8,26 @@ coin_20=int(input("How many 20Rs. coins? : "))
 l=150
 c=175
 e=200
+sp=-1
 
 money=coin_10*10 + coin_20*20+coin_5*5
+
 match(choice):
     case "latte":
-        sp=l-money
+        if money>l:
+            sp=money -l
+        else:
+            print(f"please give Rs{l-money} more")
     case "espresso":
-        sp=e-money
+        if money>e:
+            sp=money -e
+        else:
+            print(f"please give Rs{e-money} more")
     case "cappuccino":
-        sp=c-money
+        if money>c:
+            sp=money -c
+        else:
+            print(f"please give Rs{c-money} more")
 
-print(f"Here's your change of {sp} rupees")
+if sp>=0 :
+    print(f"Here's your change of {sp} rupees")
