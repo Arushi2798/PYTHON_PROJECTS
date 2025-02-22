2e907845fdecf796e948c0cf676b75657be3695a
Arushi2798 <arushi1301@gmail.com>
2025-02-22 22:15:24 +0530

    code update

--- Project_9.py
+++ Project_9.py
@@ -9,6 +9,11 @@ l=150
 c=175
 e=200
 
-def switch(choice):
-    case choice=="latte":
-        
+money=coin_10*10 + coin_20*20+coin_5*5
+match(choice):
+    case "latte":
+        sp=l-money
+    case "espresso":
+        sp=e-money
+    case "cappuccino":
+        sp=c-money
