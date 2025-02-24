45f9255b1fcbd9fa6065f599a56f972409e5df0b
Arushi2798 <arushi1301@gmail.com>
2025-02-24 12:59:34 +0530

    code clear

--- practise_problem.py
+++
@@ -1,8 +0,0 @@
-resources={"Water":500,"Milk":500, "Coffee":50, "Money":0}
-# for i in range(2):
-#     resources["coffee"]=resources.get("Coffee")-24
-#     resources["milk"]=resources.get("Milk")-150
-#     resources["water"]=resources.get("Water")-200
-#     resources['paisa']=resources.get("Money")+150
-for i in resources:
-    print(resources.values())
