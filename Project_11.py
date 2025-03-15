d16ad6292795f5947e18cd41c2f0e284b7dbb52e
Arushi2798 <arushi1301@gmail.com>
2025-03-15 12:12:43 +0530

    code update

---
+++ Project_11.py
@@ -0,0 +1,7 @@
+# # to install the dependent modules aas per the book requirements
+# import subprocess
+# import sys
+
+# subprocess.run([sys.executable, "-m", "pip", "install", "--upgrade", "pip", "setuptools", "wheel"], check=True)
+# subprocess.run([sys.executable, "-m", "pip", "install", "bigbookpython"], check=True)
+#check if insatlled correctly by importing pyperclip , bext
