7fbb9c95358c02cfdd0ee20217ed8926c6ed6f57
Arushi2798 <arushi1301@gmail.com>
2025-03-15 12:40:36 +0530

    file update

--- Project_11.py
+++ Project_11.py
@@ -8,4 +8,23 @@
 
 
 ####
-# question: 
+"""question: In Bagels, a deductive logic game, you must guess a secret three-digit number based on clues. 
+The game offers one of the following hints in response to your guess: “Pico” when your guess has a correct digit in the wrong place,
+ “Fermi” when your guess has a correct digit in the correct place, and “Bagels” if your guess has no correct digits. 
+ You have 10 tries to guess the secret number."""
+
+import random
+
+print("\nBagels, a deductive logic game.\nBy Al Sweigart al@inventwithpython.com \n\nI am thinking of a 3-digit number. Try to guess what it is.")
+print("Here are some clues: \n When I say:    That means:\n  Pico         One digit is correct but in the wrong position.\n  Fermi        One digit is correct and in the right position.\n  Bagels       No digit is correct.")
+
+number =random.randint(100,999)
+# print(number)
+
+print("\nI have thought up a number.\nYou have 10 guesses to get it.")
+
+
+
+for i in range(10):
+     guess= int(input(f'Guess #{i+1}: '))
+    
