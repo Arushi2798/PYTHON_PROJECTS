54ea8610c8feebc7d1c736205777932f04c22c2f
Arushi2798 <arushi1301@gmail.com>
2025-03-17 12:10:06 +0530

    code update

--- Project_12.py
+++ Project_12.py
@@ -7,9 +7,23 @@ in which we conduct multiple random trials to understand the likely outcomes, Mo
 
 
 import datetime, random
-print("Birthday Paradox, by Al Sweigart al@inventwithpython.com")
 
-num = input("how many birthday's should i generate? (Max 100) \n >")
+print('''Birthday Paradox, by Al Sweigart al@inventwithpython.com 
+The birthday paradox shows us that in a group of N people, the odds
+that two of them have matching birthdays is surprisingly large.
+This program does a Monte Carlo simulation (that is, repeated random
+simulations) to explore this concept.
+ 
+(It's not actually a paradox, it's just a surprising result.)''')
 
-month=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+month=('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
 # date=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+
+num = int(input("\nhow many birthday's should i generate? (Max 100) \n >"))
+
+if num <=100 and num >0:
+    pass
+else:
+    print('enter a valid number')
+
+print(f'\nHere are {num} birthdays')
