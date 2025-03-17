4db5cdd585c7fb51aad572841cadad33d4dbb1f9
Arushi2798 <arushi1301@gmail.com>
2025-03-17 13:46:30 +0530

    code update

--- Project_12.py
+++ Project_12.py
@@ -6,7 +6,23 @@ to determine the percentages for groups of different sizes. We call these types 
 in which we conduct multiple random trials to understand the likely outcomes, Monte Carlo experiments."""
 
 
-import datetime, random
+import datetime as d
+import random
+
+def getbirthdays(number):
+    dates=[]
+    for i in range(number):
+        startyear=d.date(2024,1,1)
+        randomday = d.timedelta(days=random.randint(0,365))#to generate a new date after randomly selecting a time difference between 0 to 365 days
+        caldates = startyear+randomday
+        newmonth = MONTHS[caldates.month-1]
+        newday=caldates.day
+        newdate= newmonth +" "+ str(newday)
+        dates.append(newdate)
+
+    return dates
+
+
 
 print('''Birthday Paradox, by Al Sweigart al@inventwithpython.com 
 The birthday paradox shows us that in a group of N people, the odds
@@ -16,7 +32,7 @@ simulations) to explore this concept.
  
 (It's not actually a paradox, it's just a surprising result.)''')
 
-month=('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
+MONTHS=('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
 # date=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
 
 num = int(input("\nhow many birthday's should i generate? (Max 100) \n >"))
@@ -27,3 +43,17 @@ else:
     print('enter a valid number')
 
 print(f'\nHere are {num} birthdays')
+birthdates = getbirthdays(num)
+for each in birthdates:
+    print(each, end=", ")
+ 
+"""for similar effects use following when the fucntion only returns list of dates without modifications
+for i, birthday in enumerate(birthdays):
+      if i != 0:
+          # Display a comma for each birthday after the first birthday.
+          print(', ', end='')
+      monthName = MONTHS[birthday.month - 1]
+      dateText = '{} {}'.format(monthName, birthday.day)
+      print(dateText, end='')
+  print()
+  print()"""
