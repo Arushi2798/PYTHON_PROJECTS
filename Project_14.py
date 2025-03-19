e7531a295e9ac6ba91688724848b5c05e2148d8d
Arushi2798 <arushi1301@gmail.com>
2025-03-19 19:26:40 +0530

    code update

--- Project_14.py
+++ Project_14.py
@@ -9,3 +9,25 @@ The playing cards in this program are an example of ASCII art:
 |A  | |10 |
 | ♣ | | ♦ |
 |__A| |_10|"""
+
+print("Blackjack, by Al Sweigart al@inventwithpython.com")
+print("""\nRules:
+      Try to get as close to 21 without going over.
+      Kings, Queens, and Jacks are worth 10 points.
+      Aces are worth 1 or 11 points.
+      Cards 2 through 10 are worth their face value.
+      (H)it to take another card.
+      (S)tand to stop taking cards.
+      On your first play, you can (D)ouble down to increase your bet
+      but must hit exactly one more time before standing.
+      In case of a tie, the bet is returned to the player.
+      The dealer stops hitting at 17.
+Money: 5000""")
+
+m=input("How much do you bet? (1-5000, or QUIT)\n > ")
+if m.upper()=="QUIT":
+    exit()
+else:
+    bet=int(m)
+    print(f"bet: {bet}")
+
